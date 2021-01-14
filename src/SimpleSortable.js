3b08df2e60daf66fd9a7ef24a143c89881782adf
stefanscript <sha32768@gmail.com>
2021-01-14 19:42:58 +0000

    init version with demo

---
+++ src/SimpleSortable.js
@@ -0,0 +1,46 @@
+function SimpleSortable(newOrderHandler) {
+    window.addEventListener("load", () => {
+        let dragged;
+        let draggedY;
+        let target;
+        let selectorItems = ".simple-sortable li";
+        const list = document.querySelectorAll(selectorItems);
+        list.forEach((item) => {
+            
+            item.addEventListener("dragstart", (e) => {
+                dragged = e.target;
+                draggedY = dragged.getBoundingClientRect().y;
+                dragged.style.border = "1px solid #000";
+            });
+            
+            item.addEventListener("dragenter", (e) => {
+                if (dragged !== e.target) {
+                    target = e.target;
+                }
+            });
+            
+            item.addEventListener("dragend", (e) => {
+                e.preventDefault();
+                if (target) {
+                    if (draggedY < target.getBoundingClientRect().y) {
+                        dragged.parentNode.removeChild(dragged);
+                        target.after(dragged);
+                    } else {
+                        dragged.parentNode.removeChild(dragged);
+                        target.before(dragged);
+                    }
+                }
+                
+                dragged.style.border = "0";
+                
+                let newOrder = [];
+                document.querySelectorAll(selectorItems).forEach(a => {
+                    newOrder.push(a.dataset.id);
+                });
+                newOrderHandler(newOrder);
+            }, false);
+        });
+    });
+}
+
+export default SimpleSortable;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import SimpleSortable from "./SimpleSortable";
+
+SimpleSortable(function (newOrder) {
+    console.log("print new order", newOrder)
+});
+
